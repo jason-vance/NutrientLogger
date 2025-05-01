f487969cd23cc768cadbb1ac5eefffea643c63ef
Jason Vance <jasonvance77@gmail.com>
2025-04-30 16:03:18 -1000

    Tweak hours for dashboard greeting

--- Nutrient Logger/Scenes/Dashboard/DashboardView.swift
+++ Nutrient Logger/Scenes/Dashboard/DashboardView.swift
@@ -77,9 +77,9 @@ struct DashboardView: View {
             return "Try Sleep?"
         } else if hour >= 2 && hour < 12 {
             return "Good Morning"
-        } else if hour >= 12 && hour < 16 {
+        } else if hour >= 12 && hour < 17 {
             return "Good Afternoon"
-        } else if hour >= 16 && hour < 22 {
+        } else if hour >= 17 && hour < 22 {
             return "Good Evening"
         } else {
             return "Up Late?"
