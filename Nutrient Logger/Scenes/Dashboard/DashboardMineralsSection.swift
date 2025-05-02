213f8d1ae393cb36bd2a7a3dab7ab386ed253cb0
Jason Vance <jasonvance77@gmail.com>
2025-05-01 19:55:02 -1000

    Change some minerals to only show if they have been consumed

--- Nutrient Logger/Scenes/Dashboard/DashboardMineralsSection.swift
+++ Nutrient Logger/Scenes/Dashboard/DashboardMineralsSection.swift
@@ -10,26 +10,27 @@ import SwiftUI
 //TODO: Navigate to NutrientDetailView with all of the foods containing this nutrient
 struct DashboardMineralsSection: View {
     
+    // Commented out minerals are not in any of the FDC foods (or very uncommon)
     static let orderedWhitelist: [String] = [
-        FdcNutrientGroupMapper.NutrientNumber_Boron_B,
+//        FdcNutrientGroupMapper.NutrientNumber_Boron_B,
         FdcNutrientGroupMapper.NutrientNumber_Calcium_Ca,
-        FdcNutrientGroupMapper.NutrientNumber_Chromium_Cr,
+//        FdcNutrientGroupMapper.NutrientNumber_Chromium_Cr,
         FdcNutrientGroupMapper.NutrientNumber_Copper_Cu,
-        FdcNutrientGroupMapper.NutrientNumber_Fluoride_F,
-        FdcNutrientGroupMapper.NutrientNumber_Iodine_I,
+//        FdcNutrientGroupMapper.NutrientNumber_Fluoride_F,
+//        FdcNutrientGroupMapper.NutrientNumber_Iodine_I,
         FdcNutrientGroupMapper.NutrientNumber_Iron_Fe,
         FdcNutrientGroupMapper.NutrientNumber_Magnesium_Mg,
         FdcNutrientGroupMapper.NutrientNumber_Manganese_Mn,
-        FdcNutrientGroupMapper.NutrientNumber_Molybdenum_Mo,
+//        FdcNutrientGroupMapper.NutrientNumber_Molybdenum_Mo,
         FdcNutrientGroupMapper.NutrientNumber_Phosphorus_P,
         FdcNutrientGroupMapper.NutrientNumber_Potassium_K,
         FdcNutrientGroupMapper.NutrientNumber_Selenium_Se,
         FdcNutrientGroupMapper.NutrientNumber_Sodium_Na,
         FdcNutrientGroupMapper.NutrientNumber_Zinc_Zn,
-        FdcNutrientGroupMapper.NutrientNumber_Chlorine_Cl,
-        FdcNutrientGroupMapper.NutrientNumber_Cobalt_Co,
-        FdcNutrientGroupMapper.NutrientNumber_Nickel_Ni,
-        FdcNutrientGroupMapper.NutrientNumber_Sulfur_S,
+//        FdcNutrientGroupMapper.NutrientNumber_Chlorine_Cl,
+//        FdcNutrientGroupMapper.NutrientNumber_Cobalt_Co,
+//        FdcNutrientGroupMapper.NutrientNumber_Nickel_Ni,
+//        FdcNutrientGroupMapper.NutrientNumber_Sulfur_S,
     ]
     
     let mineralsKey = FdcNutrientGroupMapper.GroupNumber_Minerals
